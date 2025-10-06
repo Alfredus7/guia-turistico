61e3bb4a8b06f2e347755217f13a372b58a71276
ESTUDIANTE <ESTUDIANTE@LAB2-PC-01>
2025-10-06 09:28:19 -0400

    primer migration

---
+++ Data/Migrations/20251006132249_m1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace guia_turistico.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tipos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipos", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SitiosTuristicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    DescripcionIngles = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    DescripcionPortugues = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Latitud = table.Column<double>(type: "float", nullable: false),
+                    Longitud = table.Column<double>(type: "float", nullable: false),
+                    TipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SitiosTuristicos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SitiosTuristicos_Tipos_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "Tipos",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comentarios",
+                columns: table => new
+                {
+                    ComentarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Texto = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Puntuacion = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SitioTuristicoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentarios", x => x.ComentarioId);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_SitiosTuristicos_SitioTuristicoId",
+                        column: x => x.SitioTuristicoId,
+                        principalTable: "SitiosTuristicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImagenesSitio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    SitioTuristicoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImagenesSitio", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ImagenesSitio_SitiosTuristicos_SitioTuristicoId",
+                        column: x => x.SitioTuristicoId,
+                        principalTable: "SitiosTuristicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_SitioTuristicoId",
+                table: "Comentarios",
+                column: "SitioTuristicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_UsuarioId",
+                table: "Comentarios",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImagenesSitio_SitioTuristicoId",
+                table: "ImagenesSitio",
+                column: "SitioTuristicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SitiosTuristicos_TipoId",
+                table: "SitiosTuristicos",
+                column: "TipoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentarios");
+
+            migrationBuilder.DropTable(
+                name: "ImagenesSitio");
+
+            migrationBuilder.DropTable(
+                name: "SitiosTuristicos");
+
+            migrationBuilder.DropTable(
+                name: "Tipos");
+        }
+    }
+}
